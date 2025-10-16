a8a39f2665ecd69c76d1184cb3505cd3eba0d8af
andria21 <andriamarqarovi33@gmail.com>
2025-10-16 13:49:02 +0400

    changed home images

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import HomeImage from "../../public/images/homescreen.jpg";
+import HomeImage from "../../public/images/option1.jpg";
 import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="relative w-full aspect-[16/9] sm:aspect-[2/1] md:aspect-[16/9] max-[500px]:aspect-auto max-[500px]:h-screen">
+    <div className="relative w-full xl:h-screen aspect-[16/9] sm:aspect-[2/1] md:aspect-[16/9] max-[500px]:aspect-auto max-[500px]:h-screen">
       <Image
         src={
-          "https://images.unsplash.com/photo-1562438668-bcf0ca6578f0?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          HomeImage
         }
         fill
         alt="Hero Image"
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,13 +6,13 @@ export default function Projects() {
     <div className="flex flex-col items-center justify-center w-full">
       <div className="grid md:grid-cols-2 grid-cols-1 md:gap-y-52 sm:gap-y-20 gap-y-14 w-full md:pt-40 pt-30 pb-20">
         <ProjectCard
-          image="https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          image="https://images.unsplash.com/photo-1724582586529-62622e50c0b3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1228"
           title="SPIRES"
           description="2025 Tbilisi, Georgia"
           isLink
         />
         <ProjectCard
-          image="https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          image="https://images.unsplash.com/photo-1653972233229-1b8c042d6d8e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1332"
           title="SPIRES"
           description="2025 Tbilisi, Georgia"
           isLink
@@ -20,14 +20,14 @@ export default function Projects() {
       </div>
       <div className="grid md:grid-cols-2 grid-cols-1 md:gap-y-52 sm:gap-y-20 gap-y-14 w-full sm:pb-50 pb-40">
         <ProjectCard
-          image="https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          image="https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170"
           title="SPIRES"
           description="2025 Tbilisi, Georgia"
           isLink
         />
 
         <ProjectCard
-          image="https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          image="https://images.unsplash.com/photo-1640357960494-9242650846d3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1043"
           title="SPIRES"
           description="2025 Tbilisi, Georgia"
           isLink
